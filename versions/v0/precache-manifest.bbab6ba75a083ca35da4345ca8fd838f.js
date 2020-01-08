c94abfa588b1e86b9cda1e52830b594cb6971efe
Sahil Kapur <sahilkapur.a@gmail.com>
2020-01-08 03:03:30 -0800

    added v0 mockup to version history

---
+++ versions/v0/precache-manifest.bbab6ba75a083ca35da4345ca8fd838f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af08f43909067deec7e6ea849fa13ff2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d66055cd73c80afd233d",
+    "url": "./static/css/main.46de12a2.chunk.css"
+  },
+  {
+    "revision": "db3f7468af107db47781",
+    "url": "./static/js/2.f959fdc9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.f959fdc9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d66055cd73c80afd233d",
+    "url": "./static/js/main.54070779.chunk.js"
+  },
+  {
+    "revision": "1317d934f26b594fb83d",
+    "url": "./static/js/runtime-main.a281e9e3.js"
+  }
+]);
