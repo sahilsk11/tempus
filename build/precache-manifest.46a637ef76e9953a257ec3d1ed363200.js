36a0cc887377f12cd781c35bc9d21f4d862e369f
Sahil Kapur <sahilkapur.a@gmail.com>
2020-01-08 02:57:49 -0800

    patched build homepage

---
+++ build/precache-manifest.46a637ef76e9953a257ec3d1ed363200.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d31d042a59fc2c36f11df043f8918cb3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "59cf4436693b96dbc06d",
+    "url": "./static/css/main.46de12a2.chunk.css"
+  },
+  {
+    "revision": "db3f7468af107db47781",
+    "url": "./static/js/2.f959fdc9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.f959fdc9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59cf4436693b96dbc06d",
+    "url": "./static/js/main.0ed6fa8a.chunk.js"
+  },
+  {
+    "revision": "1317d934f26b594fb83d",
+    "url": "./static/js/runtime-main.a281e9e3.js"
+  }
+]);
